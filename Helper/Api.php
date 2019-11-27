c131e5dfb54c241fa5c4e2ca3d5981c84a0c46c6
Palaniappan Chellappan <palaniappanc@gmail.com>
2019-11-27 15:50:03 +0530

    sync safeguards

--- Helper/Api.php
+++ Helper/Api.php
@@ -18,7 +18,7 @@ class Api extends \Magento\Framework\App\Helper\AbstractHelper
         $this->tagalysLogger = new \Zend\Log\Logger();
         $this->tagalysLogger->addWriter($writer);
 
-        $this->pluginVersion = '2.0.3';
+        $this->pluginVersion = '2.0.4';
         
         $this->cacheApiCredentials();
     }
