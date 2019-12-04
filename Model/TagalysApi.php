30629f0844e24d93cdee8475e4c96324441705b2
Shankar <shankar@tagalys.com>
2019-12-04 18:20:42 +0530

    everything under single try catch

--- Model/TagalysApi.php
+++ Model/TagalysApi.php
@@ -52,106 +52,106 @@ class TagalysApi implements TagalysManagementInterface
 
     public function info($params)
     {
-        $response = array('result' => false, 'message' => 'invalid info_type');
-        switch ($params['info_type']) {
-            case 'status':
-                try {
-                    $info = array('config' => array(), 'files_in_media_folder' => array(), 'sync_status' => $this->tagalysSync->status());
-                    $configCollection = $this->configFactory->create()->getCollection()->setOrder('id', 'ASC');
-                    foreach ($configCollection as $i) {
-                        $info['config'][$i->getData('path')] = $i->getData('value');
-                    }
-                    $mediaDirectory = $this->filesystem->getDirectoryRead('media')->getAbsolutePath('tagalys');
-                    $filesInMediaDirectory = scandir($mediaDirectory);
-                    foreach ($filesInMediaDirectory as $key => $value) {
-                        if (!is_dir($mediaDirectory . DIRECTORY_SEPARATOR . $value)) {
-                            if (!preg_match("/^\./", $value)) {
-                                $info['files_in_media_folder'][] = $value;
+        $response = array('status' => 'error', 'message' => 'invalid info_type');
+        try {
+            switch ($params['info_type']) {
+                case 'status':
+                    try {
+                        $info = array('config' => array(), 'files_in_media_folder' => array(), 'sync_status' => $this->tagalysSync->status());
+                        $configCollection = $this->configFactory->create()->getCollection()->setOrder('id', 'ASC');
+                        foreach ($configCollection as $i) {
+                            $info['config'][$i->getData('path')] = $i->getData('value');
+                        }
+                        $mediaDirectory = $this->filesystem->getDirectoryRead('media')->getAbsolutePath('tagalys');
+                        $filesInMediaDirectory = scandir($mediaDirectory);
+                        foreach ($filesInMediaDirectory as $key => $value) {
+                            if (!is_dir($mediaDirectory . DIRECTORY_SEPARATOR . $value)) {
+                                if (!preg_match("/^\./", $value)) {
+                                    $info['files_in_media_folder'][] = $value;
+                                }
                             }
                         }
+                        $response = $info;
+                    } catch (Exception $e) {
+                        $response = array('result' => false, 'exception' => true);
+                        $this->tagalysApi->log('warn', 'Error in indexAction: ' . $e->getMessage(), array('params' => $params));
                     }
-                    $response = $info;
-                } catch (Exception $e) {
-                    $response = array('result' => false, 'exception' => true);
-                    $this->tagalysApi->log('warn', 'Error in indexAction: ' . $e->getMessage(), array('params' => $params));
-                }
-                break;
-            case 'product_details':
-                $productDetails = array();
-                foreach ($this->tagalysConfiguration->getStoresForTagalys() as $storeId) {
-                    $productDetailsForStore = (array) $this->tagalysProduct->productDetails($params['product_id'], $storeId);
-                    $productDetails['store-' . $storeId] = $productDetailsForStore;
-                }
-                $response = $productDetails;
-                break;
-            case 'reset_sync_statuses':
-                $this->queueHelper->truncate();
-                foreach ($this->tagalysConfiguration->getStoresForTagalys() as $storeId) {
-                    $sync_types = array('updates', 'feed');
-                    foreach ($sync_types as $sync_type) {
-                        $syncTypeStatus = $this->tagalysConfiguration->getConfig("store:$storeId:" . $sync_type . "_status", true);
-                        $syncTypeStatus['status'] = 'finished';
-                        $feed_status = $this->tagalysConfiguration->setConfig("store:$storeId:" . $sync_type . "_status", json_encode($syncTypeStatus));
+                    break;
+                case 'product_details':
+                    $productDetails = array();
+                    foreach ($this->tagalysConfiguration->getStoresForTagalys() as $storeId) {
+                        $productDetailsForStore = (array) $this->tagalysProduct->productDetails($params['product_id'], $storeId);
+                        $productDetails['store-' . $storeId] = $productDetailsForStore;
                     }
-                }
-                $response = array('reset' => true);
-                break;
-            case 'trigger_full_product_sync':
-                $this->tagalysApi->log('warn', 'Triggering full products resync via API', array('force_regenerate_thumbnails' => ($params['force_regenerate_thumbnails'] == 'true')));
-                foreach ($this->tagalysConfiguration->getStoresForTagalys() as $storeId) {
-                    if (isset($params['products_count'])) {
-                        $this->tagalysSync->triggerFeedForStore($storeId, ($params['force_regenerate_thumbnails'] == 'true'), $params['products_count'], true);
+                    $response = $productDetails;
+                    break;
+                case 'reset_sync_statuses':
+                    $this->queueHelper->truncate();
+                    foreach ($this->tagalysConfiguration->getStoresForTagalys() as $storeId) {
+                        $sync_types = array('updates', 'feed');
+                        foreach ($sync_types as $sync_type) {
+                            $syncTypeStatus = $this->tagalysConfiguration->getConfig("store:$storeId:" . $sync_type . "_status", true);
+                            $syncTypeStatus['status'] = 'finished';
+                            $feed_status = $this->tagalysConfiguration->setConfig("store:$storeId:" . $sync_type . "_status", json_encode($syncTypeStatus));
+                        }
+                    }
+                    $response = array('reset' => true);
+                    break;
+                case 'trigger_full_product_sync':
+                    $this->tagalysApi->log('warn', 'Triggering full products resync via API', array('force_regenerate_thumbnails' => ($params['force_regenerate_thumbnails'] == 'true')));
+                    foreach ($this->tagalysConfiguration->getStoresForTagalys() as $storeId) {
+                        if (isset($params['products_count'])) {
+                            $this->tagalysSync->triggerFeedForStore($storeId, ($params['force_regenerate_thumbnails'] == 'true'), $params['products_count'], true);
+                        } else {
+                            $this->tagalysSync->triggerFeedForStore($storeId, ($params['force_regenerate_thumbnails'] == 'true'), false, true);
+                        }
+                    }
+                    $this->queueHelper->truncate();
+                    $response = array('triggered' => true);
+                    break;
+                case 'insert_into_sync_queue':
+                    $this->tagalysApi->log('warn', 'Inserting into sync queue via API', array('product_ids' => $params['product_ids']));
+                    $this->queueHelper->insertUnique($params['product_ids']);
+                    $response = array('inserted' => true);
+                    break;
+                case 'truncate_sync_queue':
+                    $this->tagalysApi->log('warn', 'Truncating sync queue via API');
+                    $this->queueHelper->truncate();
+                    $response = array('truncated' => true);
+                    break;
+                case 'mark_positions_sync_required_for_categories':
+                    $this->tagalysCategoryHelper->markPositionsSyncRequiredForCategories($params['store_id'], $params['category_ids']);
+                    $response = array('updated' => true);
+                    break;
+                case 'get_categories_powered_by_tagalys':
+                    $categories = array();
+                    $tagalysCategoryCollection = $this->tagalysCategoryFactory->create()->getCollection()->setOrder('id', 'ASC');
+                    foreach ($tagalysCategoryCollection as $i) {
+                        $fields = array('id', 'category_id', 'store_id', 'positions_synced_at', 'positions_sync_required', 'marked_for_deletion', 'status');
+                        $categoryData = array();
+                        foreach ($fields as $field) {
+                            $categoryData[$field] = $i->getData($field);
+                        }
+                        array_push($categories, $categoryData);
+                    }
+                    $response = array('categories' => $categories);
+                    break;
+                case 'update_tagalys_health_status':
+                    if (isset($params['value']) && in_array($params['value'], array('1', '0'))) {
+                        $this->tagalysConfiguration->setConfig("tagalys:health", $params['value']);
                     } else {
-                        $this->tagalysSync->triggerFeedForStore($storeId, ($params['force_regenerate_thumbnails'] == 'true'), false, true);
+                        $this->tagalysSync->updateTagalysHealth();
                     }
-                }
-                $this->queueHelper->truncate();
-                $response = array('triggered' => true);
-                break;
-            case 'insert_into_sync_queue':
-                $this->tagalysApi->log('warn', 'Inserting into sync queue via API', array('product_ids' => $params['product_ids']));
-                $this->queueHelper->insertUnique($params['product_ids']);
-                $response = array('inserted' => true);
-                break;
-            case 'truncate_sync_queue':
-                $this->tagalysApi->log('warn', 'Truncating sync queue via API');
-                $this->queueHelper->truncate();
-                $response = array('truncated' => true);
-                break;
-            case 'mark_positions_sync_required_for_categories':
-                $this->tagalysCategoryHelper->markPositionsSyncRequiredForCategories($params['store_id'], $params['category_ids']);
-                $response = array('updated' => true);
-                break;
-            case 'get_categories_powered_by_tagalys':
-                $categories = array();
-                $tagalysCategoryCollection = $this->tagalysCategoryFactory->create()->getCollection()->setOrder('id', 'ASC');
-                foreach ($tagalysCategoryCollection as $i) {
-                    $fields = array('id', 'category_id', 'store_id', 'positions_synced_at', 'positions_sync_required', 'marked_for_deletion', 'status');
-                    $categoryData = array();
-                    foreach ($fields as $field) {
-                        $categoryData[$field] = $i->getData($field);
-                    }
-                    array_push($categories, $categoryData);
-                }
-                $response = array('categories' => $categories);
-                break;
-            case 'update_tagalys_health_status':
-                if (isset($params['value']) && in_array($params['value'], array('1', '0'))) {
-                    $this->tagalysConfiguration->setConfig("tagalys:health", $params['value']);
-                } else {
-                    $this->tagalysSync->updateTagalysHealth();
-                }
-                $response = array('health_status' => $this->tagalysConfiguration->getConfig("tagalys:health"));
-                break;
-            case 'get_tagalys_health_status':
-                $response = array('health_status' => $this->tagalysConfiguration->getConfig("tagalys:health"));
-                break;
-            case 'update_tagalys_plan_features':
-                $this->tagalysConfiguration->setConfig('tagalys_plan_features', $params['plan_features'], true);
-                $response = array('updated' => true);
-                break;
-            case 'assign_products_to_category_and_remove':
-                try {
+                    $response = array('health_status' => $this->tagalysConfiguration->getConfig("tagalys:health"));
+                    break;
+                case 'get_tagalys_health_status':
+                    $response = array('health_status' => $this->tagalysConfiguration->getConfig("tagalys:health"));
+                    break;
+                case 'update_tagalys_plan_features':
+                    $this->tagalysConfiguration->setConfig('tagalys_plan_features', $params['plan_features'], true);
+                    $response = array('updated' => true);
+                    break;
+                case 'assign_products_to_category_and_remove':
                     $this->logger->info("assign_products_to_category_and_remove: params: " . json_encode($params));
                     if ($params['product_positions'] == -1) {
                         $params['product_positions'] = [];
@@ -162,12 +162,8 @@ class TagalysApi implements TagalysManagementInterface
                     } else {
                         $response = ['status' => 'error', 'message' => 'Unknown error occurred'];
                     }
-                } catch (\Exception $e) {
-                    $response = ['status' => 'error', 'message' => $e->getMessage(), 'trace' => $e->getTrace()];
-                }
-                break;
-            case 'update_product_positions':
-                try {
+                    break;
+                case 'update_product_positions':
                     $this->logger->info("update_product_positions: params: " . json_encode($params));
                     if ($params['product_positions'] == -1) {
                         $params['product_positions'] = [];
@@ -179,56 +175,48 @@ class TagalysApi implements TagalysManagementInterface
                     } else {
                         $response = ['status' => 'error', 'message' => 'Unknown error occurred'];
                     }
-                } catch (\Exception $e) {
-                    $response = ['status' => 'error', 'message' => $e->getMessage(), 'trace' => $e->getTrace()];
-                }
-                break;
-            case 'clear_category_caches':
-                try {
+                    break;
+                case 'clear_category_caches':
                     $this->logger->info("clear_category_caches: params: " . json_encode($params));
                     $this->tagalysCategoryHelper->clearCacheForCategories($params['category_ids']);
                     $response = ['status' => 'OK', 'message' => 'cleared'];
-                } catch (\Exception $e) {
-                    $response = ['status' => 'error', 'message' => $e->getMessage(), 'trace' => $e->getTrace()];
-                }
-                break;
-            case 'get_plugin_version':
-                $response = ['status' => 'OK', 'plugin_version' => $this->tagalysApi->getPluginVersion()];
-                break;
-            case 'ping':
-                $response = ['status' => 'OK', 'message' => 'pong'];
-                break;
-            case 'get_tagalys_logs':
-                try {
+                    break;
+                case 'get_plugin_version':
+                    $response = ['status' => 'OK', 'plugin_version' => $this->tagalysApi->getPluginVersion()];
+                    break;
+                case 'ping':
+                    $response = ['status' => 'OK', 'message' => 'pong'];
+                    break;
+                case 'get_tagalys_logs':
                     if (empty($params['lines'])) {
                         $params['lines'] = 10;
                     }
                     ob_start();
                     passthru('tail -n' . escapeshellarg($params['lines']) . ' var/log/tagalys_' . escapeshellarg($params['file']) . '.log');
                     $response = ['status' => 'OK', 'message' => explode("\n", trim(ob_get_clean()))];
-                } catch (\Exception $e) {
-                    $response = ['status' => 'error', 'message' => $e->getMessage(), 'trace' => $e->getTrace()];
-                }
-                break;
-            case 'update_category_pages_store_mapping':
-                $this->tagalysConfiguration->setConfig('category_pages_store_mapping', $params['store_mapping'], true);
-                $response = array('updated' => true, $params['store_mapping']);
-                break;
-            case 'update_product_update_detection_methods':
-                $this->tagalysConfiguration->setConfig('product_update_detection_methods', $params['methods'], true);
-                $response = array('updated' => true, $params['methods']);
-                break;
-            case 'set_config':
-                if (!array_key_exists('json_encode', $params)){
-                    $params['json_encode'] = false;
-                }
-                $this->tagalysConfiguration->setConfig($params['path'], $params['value'], $params['json_encode']);
-                $response = array('updated' => true, $params['value']);
-                break;
-            case 'get_order_data':
-                $res = $this->tagalysSync->getOrderData($params['store_id'], $params['from'], $params['to']);
-                $response = array('status' => 'OK', 'message' => $res);
-                break;
+                    break;
+                case 'update_category_pages_store_mapping':
+                    $this->tagalysConfiguration->setConfig('category_pages_store_mapping', $params['store_mapping'], true);
+                    $response = array('updated' => true, $params['store_mapping']);
+                    break;
+                case 'update_product_update_detection_methods':
+                    $this->tagalysConfiguration->setConfig('product_update_detection_methods', $params['methods'], true);
+                    $response = array('updated' => true, $params['methods']);
+                    break;
+                case 'set_config':
+                    if (!array_key_exists('json_encode', $params)){
+                        $params['json_encode'] = false;
+                    }
+                    $this->tagalysConfiguration->setConfig($params['path'], $params['value'], $params['json_encode']);
+                    $response = array('updated' => true, $params['value']);
+                    break;
+                case 'get_order_data':
+                    $res = $this->tagalysSync->getOrderData($params['store_id'], $params['from'], $params['to']);
+                    $response = array('status' => 'OK', 'message' => $res);
+                    break;
+            }
+        } catch (\Exception $e) {
+            $response = ['status' => 'error', 'message' => $e->getMessage(), 'trace' => $e->getTrace()];
         }
         return json_encode($response);
     }
