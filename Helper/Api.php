6ccdfc1d635dccd0f6d31de8d9ed1e9ccd78eb81
Palaniappan Chellappan <palaniappanc@gmail.com>
2020-07-24 16:58:33 +0530

    version bump

--- Helper/Api.php
+++ Helper/Api.php
@@ -20,7 +20,7 @@ class Api extends \Magento\Framework\App\Helper\AbstractHelper
         $this->tagalysLogger = new \Zend\Log\Logger();
         $this->tagalysLogger->addWriter($writer);
 
-        $this->pluginVersion = '2.0.23';
+        $this->pluginVersion = '2.0.24';
         
         $this->cacheApiCredentials();
     }
