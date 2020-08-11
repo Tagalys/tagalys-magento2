1165d0e0de7010445ed1026e5ed9f19dd5ad0211
Shankar T <shankar@tagalys.com>
2020-08-11 22:49:28 +0530

    setting secure cookie for secure frontend

--- CookieManager.php
+++ CookieManager.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Tagalys\Sync;
 
@@ -22,6 +22,11 @@ class CookieManager
     private $sessionManager;
 
     /**
+     * @param \Magento\Store\Model\StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
      * @param CookieManagerInterface $cookieManager
      * @param CookieMetadataFactory $cookieMetadataFactory
      * @param SessionManagerInterface $sessionManager
@@ -29,11 +34,13 @@ class CookieManager
     public function __construct(
         \Magento\Framework\Stdlib\CookieManagerInterface $cookieManager,
         \Magento\Framework\Stdlib\Cookie\CookieMetadataFactory $cookieMetadataFactory,
-        \Magento\Framework\Session\SessionManagerInterface $sessionManager
+        \Magento\Framework\Session\SessionManagerInterface $sessionManager,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
     ) {
         $this->cookieManager = $cookieManager;
         $this->cookieMetadataFactory = $cookieMetadataFactory;
         $this->sessionManager = $sessionManager;
+        $this->storeManager = $storeManager;
     }
 
     /**
@@ -57,6 +64,7 @@ class CookieManager
           ->createPublicCookieMetadata()
           ->setDuration($duration)
           ->setPath('/')
+          ->setSecure($this->storeManager->getStore()->isFrontUrlSecure())
           ->setDomain($this->sessionManager->getCookieDomain());
 
         $this->cookieManager->setPublicCookie(
@@ -76,6 +84,7 @@ class CookieManager
             $this->cookieMetadataFactory
                 ->createCookieMetadata()
                 ->setPath('/')
+                ->setSecure($this->storeManager->getStore()->isFrontUrlSecure())
                 ->setDomain($this->sessionManager->getCookieDomain())
         );
     }
