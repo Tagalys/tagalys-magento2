3b5fc9f000255b2b8898ffb944309970cb560c6a
Shankar T <shankar@tagalys.com>
2020-08-12 00:22:41 +0530

    set more secure cookies

--- Observer/Login.php
+++ Observer/Login.php
@@ -3,11 +3,18 @@ namespace Tagalys\Sync\Observer;
 
 class Login implements \Magento\Framework\Event\ObserverInterface
 {
+    /**
+     * @param \Magento\Store\Model\StoreManagerInterface
+     */
+    private $storeManager;
+
     public function __construct(
-        \Magento\Customer\Model\Session $session
+        \Magento\Customer\Model\Session $session,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
     )
     {
         $this->session = $session;
+        $this->storeManager = $storeManager;
     }
 
     public function execute(\Magento\Framework\Event\Observer $observer)
@@ -15,7 +22,7 @@ class Login implements \Magento\Framework\Event\ObserverInterface
         try {
             $customerId = $observer->getCustomer()->getId();
             // don't use magento's cookie helper as it sets httponly header to true
-            setcookie('__ta_logged_in', $customerId, time()+60*60*24*3, '/', $_SERVER['HTTP_HOST']);
+            setcookie('__ta_logged_in', $customerId, time()+60*60*24*3, '/', $_SERVER['HTTP_HOST'], $this->storeManager->getStore()->isFrontUrlSecure());
         } catch (\Throwable $e) {
 
         }
--- Observer/Predispatch.php
+++ Observer/Predispatch.php
@@ -3,11 +3,18 @@ namespace Tagalys\Sync\Observer;
 
 class Predispatch implements \Magento\Framework\Event\ObserverInterface
 {
+    /**
+     * @param \Magento\Store\Model\StoreManagerInterface
+     */
+    private $storeManager;
+
     public function __construct(
-        \Magento\Customer\Model\Session $session
+        \Magento\Customer\Model\Session $session,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
     )
     {
         $this->session = $session;
+        $this->storeManager = $storeManager;
     }
 
     public function execute(\Magento\Framework\Event\Observer $observer)
@@ -16,9 +23,9 @@ class Predispatch implements \Magento\Framework\Event\ObserverInterface
             if($this->session->isLoggedIn()) {
                 $customerId = $this->session->getCustomer()->getId();
                 // don't use magento's cookie helper as it sets httponly header to true
-                setcookie('__ta_user_id', $customerId, time()+60*60*24*3, '/', $_SERVER['HTTP_HOST']);
+                setcookie('__ta_user_id', $customerId, time()+60*60*24*3, '/', $_SERVER['HTTP_HOST'], $this->storeManager->getStore()->isFrontUrlSecure());
             } else {
-                setcookie("__ta_user_id", "", time()-3600, '/', $_SERVER['HTTP_HOST']);
+                setcookie("__ta_user_id", "", time()-3600, '/', $_SERVER['HTTP_HOST'], $this->storeManager->getStore()->isFrontUrlSecure());
             }
         } catch (\Throwable $e) {
 
