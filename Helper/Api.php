079c82b1877745606b258bac26a8042b6b310b63
Shankar T <shankar@tagalys.com>
2021-08-04 22:10:58 +0530

    composer json fix

--- Helper/Api.php
+++ Helper/Api.php
@@ -20,7 +20,7 @@ class Api extends \Magento\Framework\App\Helper\AbstractHelper
         $this->tagalysLogger = new \Zend\Log\Logger();
         $this->tagalysLogger->addWriter($writer);
 
-        $this->pluginVersion = '2.4.0';
+        $this->pluginVersion = '2.4.0'; // actual value: '2.4.0-beta'
 
         $this->cacheApiCredentials();
     }
