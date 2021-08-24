0545165a4f524e62dcdf4f8c861cb23f6b731cfe
Shankar T <shankar@tagalys.com>
2021-08-24 13:13:22 +0530

    record old and new parent during category move

--- Plugin/CategoryPlugin.php
+++ Plugin/CategoryPlugin.php
@@ -24,8 +24,6 @@ class CategoryPlugin
     public function afterMove(\Magento\Catalog\Model\Category $category, $result) {
         try {
             $affectedCategoryIds = $category->getAllChildren(true);
-            $json = json_encode($affectedCategoryIds);
-            $this->auditLog->logInfo("CategoryPlugin::afterMove", "Marking categories: $json as pending_sync");
             $categories = $this->tagalysCategoryFactory->create()->getCollection()
                 ->addFieldToFilter('category_id', $affectedCategoryIds)
                 ->addFieldToFilter('status', 'powered_by_tagalys')
@@ -33,7 +31,12 @@ class CategoryPlugin
             foreach ($categories as $category) {
                 $category->setStatus('pending_sync')->save();
             }
+            $this->auditLog->logInfo("CategoryPlugin::afterMove", "Marking moved categories as pending_sync", ['category_ids' => $affectedCategoryIds, 'result' => $result]);
         } catch (\Throwable $e) { }
+    }
+
+    public function beforeMove(\Magento\Catalog\Model\Category $category, $newParentId, $afterCategoryId) {
+        $this->auditLog->logInfo("CategoryPlugin::beforeMove", "Category moved", ['category_id' => $category->getId(), 'old_parent_id' => $category->getParentId(), 'new_parent_id' => $newParentId]);
     }
 }
 ?>
