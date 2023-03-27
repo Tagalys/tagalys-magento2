5ac4b4758761c3aba3196d86b1eed4293149d194
Shankar T <shankar@tagalys.com>
2023-03-27 16:46:33 +0530

    version bump

--- Helper/Api.php
+++ Helper/Api.php
@@ -23,7 +23,7 @@ class Api extends \Magento\Framework\App\Helper\AbstractHelper
 
         $this->tagalysLogger = Utils::getLogger("tagalys_log.log");
 
-        $this->pluginVersion = '2.6.0-beta3';
+        $this->pluginVersion = '2.6.0-beta4';
 
         $this->cacheApiCredentials();
     }
